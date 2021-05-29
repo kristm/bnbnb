8c0d03273ad815360da55666adaee5f97bcd3b92
Krist Menina <krist@hhww.org>
2021-05-29 21:39:58 +0800

    app helpers

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,5 @@
 class Guest < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :first_name, :last_name, :email, presence: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,11 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
+  enum status: [ :booked, :accepted, :rejected ]
+
+  validates :start_date, :end_date, :number_of_guests, presence: true
+
+
+  def nights
+    (end_date - start_date).to_i
+  end
 end
